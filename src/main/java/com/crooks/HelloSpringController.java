f04d46451002131b9ffd3d656f7e226efeb0ace3
John Crooks <johnmcrooks@gmail.com>
2016-06-20 12:08:49 -0400

    Week 6 Day 1 - In class work
    Initial Commit:
    Added Get and Post Routes
    User login/logoff

--- src/main/java/com/crooks/HelloSpringController.java
+++ src/main/java/com/crooks/HelloSpringController.java
@@ -9,14 +9,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by johncrooks on 6/20/16.
  */
 
 @Controller
 public class HelloSpringController {
+
+
     @RequestMapping(path = "/", method = RequestMethod.GET)                  //Creating the main route
-    public String home(Model model, String name, String city, Integer age){     //specifies parameters for variables to be passed  //using Integer instead of int makes it an optional argument
+    public String home(Model model, HttpSession session, String name, String city, Integer age){     //specifies parameters for variables to be passed  //using Integer instead of int makes it an optional argument
         if (name==null){
             name = "alice";
         }
@@ -29,10 +33,30 @@ public class HelloSpringController {
         }
 
         Person person = new Person(name, city, age);
+        model.addAttribute("person", person);                    //Need this line to make mustache work
 
-        model.addAttribute("person", person);
-        return "person";   //this looks for person.html even though you aren't specifically stating that you want the .html
+        String username = (String) session.getAttribute("username");  //grabs username out of the session // Has to be cast as a String because it's kinda dumb
+        User user = null;
+        if (username!=null){                                     //if username isn't null create a new user
+            user = new User(username);
+        }
+        model.addAttribute("user", user);                        //add user to the model (AKA link it to the HTML)
+        return "person";                                         //this looks for person.html template even though you aren't specifically stating that you want the .html
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(String username, HttpSession session){
+        session.setAttribute("username", username);
+        return "redirect:/";                                     //redirects back to the "/" route
 
     }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+        public String logout(HttpSession session) {
+            session.invalidate();
+            return "redirect:/";
+        }
+
+
 
 }
---
+++ src/main/java/com/crooks/User.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016.
+ */
+
+package com.crooks;
+
+/**
+ * Created by johncrooks on 6/20/16.
+ */
+public class User {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String name) {
+
+        this.name = name;
+    }
+}
